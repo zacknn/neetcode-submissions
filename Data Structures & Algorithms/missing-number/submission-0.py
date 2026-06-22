32d95bfbd7f7eb3f277814efa0c0cc080d88ea97
zaknx <150733773+zacknn@users.noreply.github.com>
2026-06-22 23:01:24 +0100

    Add: missing-number - submission-0

---
+++ Data Structures & Algorithms/missing-number/submission-0.py
@@ -0,0 +1,6 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        result = len(nums)
+        for i, num in enumerate(nums):
+            result ^= i ^ num
+        return result
